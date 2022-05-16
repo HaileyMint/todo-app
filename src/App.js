3d78f7ce196acc2ae706a947c179ada054ff37d6
hailey <peppermint7825@gmail.com>
2022-05-16 15:43:43 +0900

    todolist project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,68 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
-function App() {
-  return <div className="App">hi</div>;
-}
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초 연습중 따라타이핑하는거지만',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '어우 은근 생각할게 많고 번거롭다',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: '그래도 익숙해지면 편하긴 할 듯',
+      checked: false,
+    },
+  ]);
+
+  const nextID = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextID.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextID.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <div>
+      <TodoTemplate>
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </TodoTemplate>
+    </div>
+  );
+};
 
 export default App;
